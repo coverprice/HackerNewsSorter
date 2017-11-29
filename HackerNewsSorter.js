70ff5efa5b37dc6293b9980ac5790f2d8abaf986
International Hobo <internationalhobo@gmail.com>
2017-11-29 09:16:05 -0800

    refactor for clearer code. Fixed bug where ads broke sorter

--- HackerNewsSorter.js
+++ HackerNewsSorter.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sort Hacker News article list by points
 // @namespace    HN
-// @version      0.1
+// @version      0.2
 // @description  Sorts all HackerNews entries by article points
 // @author       jamesrussell1911@gmail.com
 // @match        https://news.ycombinator.com/
@@ -11,48 +11,71 @@
 
 (function() {
   'use strict';
+  sortArticlesByPoints();
 
-  let tbody = document.querySelector('.itemlist tbody');
+  function sortArticlesByPoints() {
+    let tbody = findArticleTbody();
+    let table_rows = findArticles(tbody);
+    sortArticles(table_rows.articles);
+    reinjectArticlesIntoDOM(tbody, table_rows.articles);
+    reinjectNonArticlesIntoDOM(tbody, table_rows.non_article_rows);
+  }
 
-  // An article is rendered as 3 table rows (class='athing' is the article title, next is the score + metadata,
-  // and next is a spacer. (Do these "hackers" know about CSS...?)
-  // This code iterates over the rows and groups article rows together, and also records non-article rows.
+  function findArticleTbody() {
+    return document.querySelector('.itemlist tbody');
+  }
 
-  let articles = Array();
-  let non_article_rows = Array();
-  for(let el of tbody.children) {
-    if(el.tagName === 'TR') {
-      if(el.className === 'athing') {
-       articles.push({
-         rows: [el],
-         points: null,
-       });
-      } else if(el.className === 'spacer') {
-        articles[articles.length-1].rows.push(el);
-      } else {
-        let score_el = el.querySelector('span.score');
-        if(score_el === null) {
-          non_article_rows.push(el);
+  function findArticles(tbody) {
+    // An article is rendered as 3 table rows (class='athing' is the article title, next is the score + metadata,
+    // and next is a spacer. (Do these "hackers" know about CSS...?)
+    // This code iterates over the rows and groups article rows together, and also records non-article rows.
+    let result = {
+      non_article_rows: Array(),
+      articles: Array(),
+    };
+    for(let el of tbody.children) {
+      if(el.tagName === 'TR') {
+        if(el.className === 'athing') {
+           result.articles.push([el]);
+        } else if(el.className === 'spacer') {
+          result.articles[result.articles.length-1].push(el);
         } else {
-          articles[articles.length-1].rows.push(el);
-          articles[articles.length-1].points = parseInt(/(\d+) point/.exec(score_el.textContent)[1], 10);
+          if(el.textContent.includes(" ago")) {
+            result.articles[result.articles.length-1].push(el);
+          } else {
+            result.non_article_rows.push(el);
+          }
         }
+      }
+    }
+    return result;
+  }
+
+  function sortArticles(articles) {
+     // Each article is an array with 3 rows. The 2nd row contains the points.
+     articles.sort((a, b) => getPoints(b) - getPoints(a)); // Descending order
+
+     function getPoints(article) {
+        let score_el = article[1].querySelector('span.score');
+        return score_el ? parseInt(/(\d+) point/.exec(score_el.textContent)[1], 10) : -1000;
+     }
+  }
+
+  function reinjectArticlesIntoDOM(tbody, articles) {
+    // NB: appendChild will *move* the DOM element to the end of the children, which is why we don't
+    // need to explicitly remove it from its current position.
+    for(let article of articles) {
+      for(let row of article) {
+        tbody.appendChild(row);
       }
     }
   }
 
-  articles.sort((a, b) => b.points - a.points); // Descending order
-
-  // NB: appendChild will *move* the DOM element to the end of the children, which is why we don't
-  // need to explicitly remove it from its current position.
-  for(let article of articles) {
-    for(let row of article.rows) {
-        tbody.appendChild(row);
-    }
-  }
   // The table had some additional non-article rows that have now bubbled to the top, so pluck them out
   // and move them to the end.
-  for(let row of non_article_rows) {
-    tbody.appendChild(row);
+  function reinjectNonArticlesIntoDOM(tbody, non_article_rows) {
+    for(let row of non_article_rows) {
+      tbody.appendChild(row);
+    }
   }
 })();
